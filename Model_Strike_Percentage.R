82a92b397477b1135a5fc6e022cab15b4af12fda
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2021-11-30 14:41:07 -0700

    Strike zone visualizations

--- Model_Strike_Percentage.R
+++ Model_Strike_Percentage.R
@@ -10,13 +10,37 @@ strike_zone <- ggplot(NULL, aes(x = plate_x, y = plate_z)) +
               ymin = 1.5,
               ymax = 3.6, color = "navyblue", alpha = 0) + 
     coord_equal() + 
-    scale_x_continuous("Horizontal location (ft.)",
+    scale_x_continuous("Horizontal pitch location (ft.)",
                        limits = c(-2, 2)) +
-    scale_y_continuous("Vertical location (ft.)",
+    scale_y_continuous("Vertical pitch location (ft.)",
                        limits = c(0, 5)) +
-    theme_bw()
+    theme_light() + 
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(), 
+          axis.line = element_line(color = 'black'))
 
 strike_zone %+% sample_n(caught_pitches, 10000) + 
     aes(color = type) + 
     geom_point(alpha = .3) + 
     scale_color_manual(values = c("gray", "navyblue"))
+
+library(mgcv)
+model <- gam(type == 'S' ~ s(plate_x, plate_z) + release_speed, family = binomial, data = sample_n(caught_pitches, 100000))
+summary(model)
+
+#preds <- predict(model, newdata = sample_n(caught_pitches, 100000), type = "response")
+library(broom)
+hats <- model %>%
+    augment(type.predict = "response")
+
+## ----gam_k_zone, warning=FALSE, fig.cap="Estimated strike probability for taken pitches using a generalized additive model. "----
+strike_zone %+% sample_n(hats, 50000) +
+    geom_point(aes(color = .fitted), alpha = 0.1) + 
+    scale_color_gradient(low = "gray70", high = "navyblue")
+
+strike_zone %+% sample_n(hats, 50000) +
+    geom_point(aes(color = .se.fit), alpha = 0.1) + 
+    scale_color_gradient(low = "gray70", high = "navyblue")
+
+head(caught_pitches)
