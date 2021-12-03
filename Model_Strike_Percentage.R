6517532829045734b9b0f6c6b3b8d911ee3d6c3d
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2021-12-02 20:28:33 -0700

    Finish work on models and begin presentation

--- Model_Strike_Percentage.R
+++ Model_Strike_Percentage.R
@@ -1,14 +1,19 @@
 library(tidyverse)
 data_2021 <- read_csv("statcast_2021.csv")
-
-caught_pitches <- data_2021 %>% filter(description %in% c("ball", "called_strike")) %>% filter(fielder_2 != 1000000000)
-
+umpires <- read_csv("plate_umpires_2021.csv")
+catchers <- read_csv("catcher_ids.csv")
+caught_pitches <- data_2021 %>% 
+    filter(description %in% c("ball", "called_strike")) %>% 
+    filter(fielder_2 != 1000000000) %>%
+    inner_join(umpires, by = 'game_pk') %>%
+    inner_join(catchers, by = c("fielder_2" = 'key_mlbam'))
+    
 plate_width <- 17 + 2 * (9/pi)
 strike_zone <- ggplot(NULL, aes(x = plate_x, y = plate_z)) + 
     geom_rect(xmin = -(plate_width/2)/12,
               xmax = (plate_width/2)/12,
               ymin = 1.5,
-              ymax = 3.6, color = "navyblue", alpha = 0) + 
+              ymax = 3.5, color = "black", alpha = 0) + 
     coord_equal() + 
     scale_x_continuous("Horizontal pitch location (ft.)",
                        limits = c(-2, 2)) +
@@ -20,11 +25,14 @@ strike_zone <- ggplot(NULL, aes(x = plate_x, y = plate_z)) +
           panel.border = element_blank(), 
           axis.line = element_line(color = 'black'))
 
-strike_zone %+% sample_n(caught_pitches, 10000) + 
+strike_zone %+% sample_n(caught_pitches, 7000) + 
     aes(color = type) + 
-    geom_point(alpha = .3) + 
-    scale_color_manual(values = c("gray", "navyblue"))
+    geom_point(alpha = .5) + 
+    scale_color_manual("Pitch Result", 
+                       values = c("darkgoldenrod", "royalblue4"), 
+                       labels = c("Ball", "Strike"))
 
+ggsave("pitches.jpg")
 zones <- caught_pitches %>%
     group_by(zone) %>%
     summarize(
@@ -54,7 +62,7 @@ p
 
 library(mgcv)
 model_data <- sample_n(caught_pitches, 100000)
-model <- gam(type == 'S' ~ s(plate_x, plate_z) + factor(stand) + factor(p_throws), family = binomial, data = model_data)
+model <- gam(type == 'S' ~ s(plate_x, plate_z) + stand + p_throws, family = binomial, data = model_data)
 model2 <- gam(type == 'S' ~ s(plate_x, plate_z) + release_speed, family = binomial, data = model_data)
 summary(model)
 anova(model2, model)
@@ -62,31 +70,38 @@ AIC(model)
 AIC(model2)
 #preds <- predict(model, newdata = sample_n(caught_pitches, 100000), type = "response")
 library(broom)
+library(modelr)
 hats <- model %>%
     augment(newdata = caught_pitches, type.predict = "response")
 
-head(hats)
+grid <- caught_pitches %>%
+    data_grid(plate_x = seq(-2, 2, length.out = 100),
+              plate_z = seq(0, 5, length.out = 100),
+              p_throws, stand)
+grid_hat <- model %>%
+    augment(newdata = grid, type.predict = "response")
+head(grid_hat)
 ## ----gam_k_zone, warning=FALSE, fig.cap="Estimated strike probability for taken pitches using a generalized additive model. "----
 
 strike_zone %+% sample_n(hats, 50000) +
     geom_point(aes(color = .fitted), alpha = 0.1) + 
     scale_color_gradient(low = "gray", high = "red")
 
-strike_zone %+% sample_n(hats, 50000) +
-    geom_point(aes(color = .se.fit), alpha = 0.5) + 
-    scale_color_gradient(low = "gray", high = "navyblue")
+strike_zone %+% grid_hat +
+    geom_tile(aes(fill= .fitted), alpha = 0.7) + 
+    scale_fill_gradient(low = "white", high = "navyblue")
 
-strike_zone %+% sample_n(hats, 50000) +
-    geom_point(aes(color = .se.fit), alpha = 0.1) + 
-    scale_color_gradient(low = "gray", high = "navyblue")
+strike_zone %+% grid_hat +
+    geom_tile(aes(fill = .se.fit), alpha = 0.7) + 
+    scale_fill_gradient(low = "gray70", high = "navyblue")
 
-head(caught_pitches)
 library(pitchRx)
 
 library(lme4)
 
-mixed_1 <- glmer(type == "S" ~ .fitted + (1|fielder_2) + (1|pitcher) + (1|stand) + (1|inning_topbot) + (1|.fitted:fielder_2),
+mixed_1 <- glmer(type == "S" ~ .fitted + (1|fielder_2) + (1|pitcher) + (1|stand) + (1|inning_topbot),
                data = sample_n(hats, 100000), family = binomial, nAGQ = 0)
+
 summary(mixed_1)
 View(hats[1:6, ])
 library(modelr)
@@ -101,4 +116,4 @@ random.effects(mixed_1) %>% data.frame() %>%
     inner_join(catchers, by = c("grp" = 'key_mlbam')) %>% arrange(condval) %>%
     head(10)
 r_eff
-
+?glmer()
