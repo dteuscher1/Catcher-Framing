98762220d40313c71fa671c649bd8e22ac1da830
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2021-12-04 21:58:28 -0700

    Finish presentation

--- Model_Strike_Percentage.R
+++ Model_Strike_Percentage.R
@@ -1,12 +1,13 @@
 library(tidyverse)
 data_2021 <- read_csv("statcast_2021.csv")
 umpires <- read_csv("plate_umpires_2021.csv")
-catchers <- read_csv("catcher_ids.csv")
+catchers <- read_csv("catcher_ids.csv") 
 caught_pitches <- data_2021 %>% 
     filter(description %in% c("ball", "called_strike")) %>% 
     filter(fielder_2 != 1000000000) %>%
     inner_join(umpires, by = 'game_pk') %>%
-    inner_join(catchers, by = c("fielder_2" = 'key_mlbam'))
+    inner_join(catchers, by = c("fielder_2" = 'key_mlbam')) %>%
+    mutate(fielder_2 = factor(fielder_2))
     
 plate_width <- 17 + 2 * (9/pi)
 strike_zone <- ggplot(NULL, aes(x = plate_x, y = plate_z)) + 
@@ -39,12 +40,9 @@ ggsave("pitches.jpg")
 library(mgcv)
 model_data <- sample_n(caught_pitches, 100000)
 model <- gam(type == 'S' ~ s(plate_x, plate_z) + stand + p_throws, family = binomial, data = model_data)
-model2 <- gam(type == 'S' ~ s(plate_x, plate_z) + release_speed, family = binomial, data = model_data)
+model2 <- gam(type == 'S' ~ s(plate_x, plate_z) + stand + p_throws + release_speed + release_spin_rate, family = binomial, data = model_data)
 summary(model)
 anova(model2, model)
-AIC(model)
-AIC(model2)
-#preds <- predict(model, newdata = sample_n(caught_pitches, 100000), type = "response")
 library(broom)
 library(modelr)
 hats <- model %>%
@@ -56,7 +54,6 @@ grid <- caught_pitches %>%
               p_throws, stand)
 grid_hat <- model %>%
     augment(newdata = grid, type.predict = "response")
-## ----gam_k_zone, warning=FALSE, fig.cap="Estimated strike probability for taken pitches using a generalized additive model. "----
 
 strike_zone %+% grid_hat +
     geom_tile(aes(fill = .fitted), alpha = 0.4) + 
@@ -68,29 +65,50 @@ strike_zone %+% grid_hat +
     scale_fill_gradient("Standard Errors", low = "gray", high = "deepskyblue4")
 ggsave("se_prob.jpg")
 
-strike_zone %+% grid_hat +
-    geom_tile(aes(fill = .se.fit), alpha = 0.7) + 
-    scale_fill_gradient(low = "gray70", high = "navyblue")
-
-library(pitchRx)
 
 library(lme4)
 
-mixed_1 <- glmer(type == "S" ~ .fitted + (1|fielder_2) + (1|pitcher) + (1|stand) + (1|inning_topbot),
-               data = sample_n(hats, 100000), family = binomial, nAGQ = 0)
+mixed_1 <- glmer(type == "S" ~ .fitted  + (1|fielder_2) + (1|pitcher) + (1|name),
+               data = hats, family = binomial, nAGQ = 0)
 
 summary(mixed_1)
-View(hats[1:6, ])
-library(modelr)
 
 catchers <- read.csv("catcher_ids.csv") %>%
     mutate(key_mlbam = factor(key_mlbam))
-random.effects(mixed_1) %>% data.frame() %>%
+
+umpires_total <- umpires %>%
+    mutate(name = factor(name)) %>% 
+    select(name) %>% unique()
+random_effects <- random.effects(mixed_1) %>% data.frame()
+
+catcher_large <- random_effects %>% filter(grpvar == "fielder_2") %>%
     inner_join(catchers, by = c("grp" = 'key_mlbam')) %>% arrange(desc(condval)) %>%
     head(10)
 
-random.effects(mixed_1) %>% data.frame() %>%
+catcher_small <- random_effects %>% filter(grpvar == "fielder_2") %>%
     inner_join(catchers, by = c("grp" = 'key_mlbam')) %>% arrange(condval) %>%
     head(10)
-r_eff
-?glmer()
+
+umpire_large <- random_effects %>% filter(grpvar == "name") %>%
+    inner_join(umpires_total, by = c("grp" = 'name')) %>% arrange(desc(condval)) %>%
+    head(10)
+
+umpire_small <- random_effects %>% filter(grpvar == "name") %>%
+    inner_join(umpires_total, by = c("grp" = 'name')) %>% arrange(condval) %>%
+    head(10)
+
+umpire_results <- umpire_large %>% bind_rows(umpire_small) %>%
+    select(grp, condval) %>%
+    rename(Umpire = grp)
+
+names(umpire_results) <- c("Umpire", "Random Effect")
+knitr::kable(umpire_results)
+
+catcher_results <- catcher_large %>% bind_rows(catcher_small) %>%
+    select(grp, condval) %>%
+    rename(Catcher = grp)
+
+names(catcher_results) <- c("Catcher", "Random Effect")
+knitr::kable(catcher_results)
+
+    
