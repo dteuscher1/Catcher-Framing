253dd0bf302087b5b52c1e669361a0c23345fed2
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2021-12-19 10:11:19 -0700

    Clean up code

--- Catcher_ids.R
+++ Catcher_ids.R
@@ -1,12 +1,29 @@
-library(tidyverse)
-library(baseballr)
+# Data of most recent change: 19.12.21
+# Author of script: David Teuscher
+# This script looks up the name for each catcher in 2021 according to their MLBAM id
+######################################
 
+# Load packages
+library(tidyverse) # Data manipulation and joining
+library(baseballr) # Functions to get the player information based on ID
+
+# Load in 2021 Statcast data
 data_2021 <- read_csv("statcast_2021.csv")
-caught_pitches <- data_2021 %>% filter(description %in% c("ball", "called_strike")) %>% filter(fielder_2 != 1000000000)
 
+# Filter to include only pitches the batter didn't swing at and where the
+# catcher is correctly recorded
+caught_pitches <- data_2021 %>% 
+    filter(description %in% c("ball", "called_strike")) %>% 
+    filter(fielder_2 != 1000000000)
 
+# Get the ID for all of the catchers
 ids <- unique(caught_pitches$fielder_2)
+
+# Create an empty data frame to include the catcher information
 catchers <- data.frame()
+
+# For each ID, look up the player and get his information and add it to the
+# data frame. A 1 second pause is added between each iteration.
 for (i in 1:length(ids)){
     id <- ids[i]
     data <- playername_lookup(id)
@@ -14,4 +31,5 @@ for (i in 1:length(ids)){
     Sys.sleep(1)
 }
 
-write_csv(catchers, "catcher_ids.csv")
+# Write the data to a .csv file, if desired
+#write_csv(catchers, "catcher_ids.csv")
